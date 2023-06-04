f2a2634a21bff8bcc1f70524a4b032eaf5551c69
CompJ <compj@ladmins-MacBook-Air-5.local>
2023-06-04 18:36:19 +0200

    first draft app

--- bags_app/app.py
+++ bags_app/app.py
@@ -1,4 +1,33 @@
 import streamlit as st
 import pandas as pd
+# import functions from separate file
+from utils import get_shop, median
 
-st.write('hello there')
+# layout and titling
+st.set_page_config(layout="wide")
+st.title('Vanishing bags')
+
+# sidebar titling
+with st.sidebar:
+    st.header('Single-use plastic bags in England, the first six years')
+    st.write(
+        "The money each company collected, once single-use plastic bag charges came in (2016)"
+    )
+
+# assign data to df
+df = pd.read_csv('bags.csv')
+
+# title bar chart then show by calling function in utils.py, and add divider
+st.header('Median value of money and donations')
+st.bar_chart(data=median(df))
+st.divider()
+
+# build sidebar for filter
+with st.sidebar:
+    shops = get_shop(df)
+    siopa = st.selectbox("Company", ["All"] + shops)
+if siopa != "All":
+    df = df[df["Company"] == siopa]
+
+# show table (filtered or not)
+st.table(df)
---
+++ bags_app/utils.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import streamlit as st
+
+DATA_FILEPATH = "bags.csv"
+
+# functions
+def get_shop(df):
+    """Returns the list of shops in the dataset."""
+    shop = df["Company"].unique()
+    return sorted(shop)
+
+def median(df):
+    """gets median on each of the variables"""
+    med = df[['Net proceeds  (£)', 'Donated to good causes (£)', 'Not donated (£)']].median()
+    return med
