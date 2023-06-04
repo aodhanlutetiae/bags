298897a5d38b36ebb0df34759b3e672b252bf4cb
CompJ <compj@ladmins-MacBook-Air-5.local>
2023-06-04 19:29:19 +0200

    more path

--- bags_app/utils.py
+++ bags_app/utils.py
@@ -1,11 +1,21 @@
 import pandas as pd
 import streamlit as st
+import os
 
-DATA_FILEPATH = "data/bags.csv"
+# get the absolute path to the directory contain the .csv file
+dir_name = os.path.abspath(os.path.dirname(__file__))
+# join the bobrza1.csv to directory to get file path
+location = os.path.join(dir_name, 'bags.csv')
 
-def load_data(data_filepath=DATA_FILEPATH):
+def load_data(data_filepath=location):
     df = pd.read_csv(data_filepath)
     return df
+
+# DATA_FILEPATH = "bags.csv"
+
+# def load_data(data_filepath=DATA_FILEPATH):
+#     df = pd.read_csv(data_filepath)
+#     return df
 
 def get_shop(df):
     """Returns the list of shops in the dataset."""
